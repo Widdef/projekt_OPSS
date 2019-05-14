c24ae02846b5d921c06e77ac086605ca69d27b0d
Maciej Żygowski <mzygowski@hotmail.com>
2019-05-14 11:29:59 +0200

    Dodanie rysowania, funkcja pobierajaca wartosc z klawiatury

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 var canvas = document.querySelector("#mario");
 var ctx = canvas.getContext('2d');
 
+function keyDownHandler(e){
+
+}
+function keyUpHandler(e) {
+    var kod = e.keyCode;
+    
+}
+function genplayer() {
+    this.size = 3;
+    this.x = 40;
+    this.y = 40;
+    this.vx = 0;
+    this.vy = 0;
+    this.color = `rgb(120,40,255)`;
+    this.rysowanie = function(){
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
+        ctx.stroke();
+        ctx.fill();
+    }
+}
+var player = new genplayer;
+player.rysowanie();
+document.addEventListener("keydown", keyUpHandler);
+document.addEventListener("keyup", keyUpHandler);
