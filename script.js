513ead590f589bd94f65d1ce97baa9d4cb111cd8
Maciej Żygowski <mzygowski@hotmail.com>
2019-05-16 23:53:24 +0200

    minimalna zmiana js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var canvas = document.querySelector("#mario");
 var ctx = canvas.getContext('2d');
 
+var canvas_style = window.getComputedStyle(canvas);
+var can_width = canvas_style.width;
+var can_height = canvas_style.height;
+
 function keyDownHandler(e){
 
 }
@@ -10,8 +14,8 @@ function keyUpHandler(e) {
 }
 function genplayer() {
     this.size = 3;
-    this.x = 40;
-    this.y = 40;
+    this.x = 4;
+    this.y = 4;
     this.vx = 0;
     this.vy = 0;
     this.color = `rgb(120,40,255)`;
@@ -23,7 +27,27 @@ function genplayer() {
         ctx.fill();
     }
 }
+function plansza(){
+    this.x;
+    this.y;
+    this.lenght;
+    this.height;
+    this.color = `rgb(0,255,0)`;
+    this.rysowanie = function(){
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.rect(this.x,this.y,this.lenght,this.height);
+        ctx.stroke();
+        ctx.fill();
+    }
+}
 var player = new genplayer;
 player.rysowanie();
+var field = new plansza;
+field.x = 0;
+field.y = 1260;
+field.lenght = 800;
+field.height = 20;
+field.rysowanie();
 document.addEventListener("keydown", keyUpHandler);
 document.addEventListener("keyup", keyUpHandler);
