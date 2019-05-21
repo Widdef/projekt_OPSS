75eed7031ef4b8a414b24038465fda8e8e5efd05
Maciej Żygowski <mzygowski@hotmail.com>
2019-05-21 11:49:11 +0200

    Działa poruszanie funkcja skoku do dokonczenia

--- script.js
+++ script.js
@@ -5,32 +5,37 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const can_width = canvas.width;
 const can_height = canvas.height;
+const dt = 10;
 
 function keyDownHandler(e){
     var kod = e.keyCode;
     switch(kod)
     {
-        case 39: player.vx += 1; break; // RIGHT
-        case 37: player.vx -= 1; break; // LEFT
-        case 38: player.vy -= 1; break; // UP
-        case 40: player.vy += 1; break; // DOWN
+        case 39: player.vx = 1; break; // RIGHT
+        case 37: player.vx = -1; break; // LEFT
+        case 38: player.vy = -1; verlet(player.x,player.y,player.vx,player.vy); break; // UP
+        case 40: player.vy = 1; break; // DOWN
     }
 }
 
-/*function keyUpHandler(e) {
+function keyUpHandler(e) {
     var kod = e.keyCode;
     switch(kod)
     {
-        case 39: alert("1"); break; // RIGHT
-        case 37: alert("a2"); break; // LEFT
-        case 38: alert("3"); break; // UP
-        case 40: alert("4"); break; // DOWN
+        case 39: player.vx = 0; break; // RIGHT
+        case 37: player.vx = 0; break; // LEFT
+        case 38: player.vy = 0; break; // UP
+        case 40: player.vy = 0; break; // DOWN
     }
-}*/
+}
 
-function verlet(r,v,dt){
-    r = r + v * dt + 5 * dt*dt;
-    v = v + 10 * dt;
+function verlet(rx,ry,vy){
+    let dt_p = dt / 1000;
+    do{
+        rx = rx + player.vx * dt_p + 5 * dt_p*dt_p;
+        ry = ry + vy * dt_p + 5 * dt_p*dt_p;
+        vy = vy - 10 * dt_p;
+    }while(ry>field.y);
 }
 //import game_element from "./game_element.js";
 
@@ -50,30 +55,11 @@ player.y = field.y - player.size;
 player.life = 3;
 player.rysowanie(0);
 
-var i = 0;
 document.addEventListener("keydown", keyDownHandler);
-//document.addEventListener("keyup", keyUpHandler);
+document.addEventListener("keyup", keyUpHandler);
 window.setInterval(function(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.move();
     player.rysowanie(0);
     field.rysowanie(1);
-    i++;
-},1000);
-if(player.x < 0 || player.y < 0 || player.x > can_width || player.y > field.y)
-{
-    alert("cos nie tak");
-    player.life--;
-}
-/*while(player.life)
-{
-    setTimeout(function(){
-        ctx.clearRect(0, 0, canvas.width, can_height - field.height);
-        player.rysowanie();
-        i++;
-    },100*i);
-    if(i == 10000)
-    {
-        player.life = 0;
-    }
-}*/
+},dt);
